0746070a7cec75d7e354b0658a6fbfbdf81ecc55
মোবাশ্বের আলম <190139.cse@student.just.edu.bdgit config --global user.name mobasheralam12345>
2024-11-30 22:53:31 +0600

    updated

--- src/pages/Shared/Secret/Secret.jsx
+++ src/pages/Shared/Secret/Secret.jsx
@@ -4,7 +4,7 @@ const Secret = () => {
         <div className="">
             <h2>Secret Things</h2>
             <div>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem dolorem quod porro non maxime amet neque, ipsam sunt tempora sint. Provident perferendis id veniam sunt. Et omnis sint recusandae fugit!
+                Moba ipsum 
             </div>
         </div>
     );
